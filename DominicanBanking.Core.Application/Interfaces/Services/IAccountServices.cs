2deb432d14418005b6c2890fb263cb839c8e1e20
Jean Carlos Reyes Encarnacion <yacalo45@hotmail.es>
2022-07-15 01:38:47 -0400

    Generic Repository and Session Helper

---
+++ DominicanBanking.Core.Application/Interfaces/Services/IAccountServices.cs
@@ -0,0 +1,15 @@
+﻿using DominicanBanking.Core.Application.DTOS.Account;
+using System.Threading.Tasks;
+
+namespace DominicanBanking.Core.Application.Interfaces.Services
+{
+    public interface IAccountServices
+    {
+        Task<ActivateResponse> ActivateAsync(ActivateRequest request);
+        Task<AuthenticationResponse> AuthenticateAsync(AuthenticationRequest request);
+        Task<ActivateResponse> DeactivateAsync(ActivateRequest request);
+        Task LogOutAsync();
+        Task<RegisterResponse> RegisterAdministratorAsync(RegisterRequest request);
+        Task<RegisterResponse> RegisterClientUserAsync(RegisterRequest request);
+    }
+}
--- DominicanBanking.Infrastructure.Identity/Services/AccountServices.cs
+++ DominicanBanking.Infrastructure.Identity/Services/AccountServices.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace DominicanBanking.Infrastructure.Identity.Services
 {
-    public class AccountServices
+    public class AccountServices : IAccountServices
     {
 
         private readonly UserManager<BankUsers> _userManager;
@@ -26,13 +26,14 @@ namespace DominicanBanking.Infrastructure.Identity.Services
             _emailServices = emailServices;
         }
 
-        public async Task<AuthenticationResponse> AuthenticateAsync(AuthenticationRequest request) {
+        public async Task<AuthenticationResponse> AuthenticateAsync(AuthenticationRequest request)
+        {
 
             AuthenticationResponse response = new();
 
             var user = await _userManager.FindByEmailAsync(request.Email);
 
-            if (user==null)
+            if (user == null)
             {
                 response.HasError = true;
                 response.Error = $"You don't have an account with this email {request.Email}";
@@ -70,39 +71,42 @@ namespace DominicanBanking.Infrastructure.Identity.Services
             return response;
         }
 
-        public async Task LogOutAsync() {
+        public async Task LogOutAsync()
+        {
             await _signInManager.SignOutAsync();
         }
 
 
-        public async Task<RegisterResponse> RegisterClientUserAsync(RegisterRequest request) {
+        public async Task<RegisterResponse> RegisterClientUserAsync(RegisterRequest request)
+        {
             RegisterResponse response = new() { HasError = false };
 
             var userWithSameUserName = await _userManager.FindByNameAsync(request.Username);
 
-            if (userWithSameUserName!=null)
+            if (userWithSameUserName != null)
             {
                 response.HasError = true;
                 response.Error = $"The username {request.Username} has been taken.";
                 return response;
             }
-            
-            var userWithSameEmail= await _userManager.FindByEmailAsync(request.Email);
 
-            if (userWithSameEmail!=null)
+            var userWithSameEmail = await _userManager.FindByEmailAsync(request.Email);
+
+            if (userWithSameEmail != null)
             {
                 response.HasError = true;
                 response.Error = $"The Email {request.Email} already registered.";
                 return response;
             }
 
-            var user = new BankUsers { 
-            Email = request.Email,
-            Name = request.Name,
-            LastName = request.LastName,
-            PhoneNumber = request.Phone,
-            UserName = request.Username,
-            Documents = request.Documents
+            var user = new BankUsers
+            {
+                Email = request.Email,
+                Name = request.Name,
+                LastName = request.LastName,
+                PhoneNumber = request.Phone,
+                UserName = request.Username,
+                Documents = request.Documents
             };
 
             var result = await _userManager.CreateAsync(user, request.Password);
@@ -119,37 +123,39 @@ namespace DominicanBanking.Infrastructure.Identity.Services
             }
 
             return response;
-        
+
         }
 
-        public async Task<RegisterResponse> RegisterAdministratorAsync(RegisterRequest request) {
+        public async Task<RegisterResponse> RegisterAdministratorAsync(RegisterRequest request)
+        {
             RegisterResponse response = new() { HasError = false };
 
             var userWithSameUserName = await _userManager.FindByNameAsync(request.Username);
 
-            if (userWithSameUserName!=null)
+            if (userWithSameUserName != null)
             {
                 response.HasError = true;
                 response.Error = $"The username {request.Username} has been taken.";
                 return response;
             }
-            
-            var userWithSameEmail= await _userManager.FindByEmailAsync(request.Email);
 
-            if (userWithSameEmail!=null)
+            var userWithSameEmail = await _userManager.FindByEmailAsync(request.Email);
+
+            if (userWithSameEmail != null)
             {
                 response.HasError = true;
                 response.Error = $"The Email {request.Email} already registered.";
                 return response;
             }
 
-            var user = new BankUsers { 
-            Email = request.Email,
-            Name = request.Name,
-            LastName = request.LastName,
-            PhoneNumber = request.Phone,
-            UserName = request.Username,
-            Documents = request.Documents
+            var user = new BankUsers
+            {
+                Email = request.Email,
+                Name = request.Name,
+                LastName = request.LastName,
+                PhoneNumber = request.Phone,
+                UserName = request.Username,
+                Documents = request.Documents
             };
 
             var result = await _userManager.CreateAsync(user, request.Password);
@@ -166,19 +172,20 @@ namespace DominicanBanking.Infrastructure.Identity.Services
             }
 
             return response;
-        
+
         }
 
-        public async Task<ActivateResponse> ActivateAsync(ActivateRequest request) {
+        public async Task<ActivateResponse> ActivateAsync(ActivateRequest request)
+        {
 
             ActivateResponse response = new()
             {
-            HasError = false
+                HasError = false
             };
 
             var user = await _userManager.FindByIdAsync(request.UserId);
 
-            if (user==null)
+            if (user == null)
             {
                 response.HasError = true;
                 response.Error = $"The user with this Id {request.UserId} doesn't exist";
@@ -190,19 +197,20 @@ namespace DominicanBanking.Infrastructure.Identity.Services
             await _userManager.UpdateAsync(user);
 
             return response;
-        
+
         }
 
-        public async Task<ActivateResponse> DeactivateAsync(ActivateRequest request) {
+        public async Task<ActivateResponse> DeactivateAsync(ActivateRequest request)
+        {
 
             ActivateResponse response = new()
             {
-            HasError = false
+                HasError = false
             };
 
             var user = await _userManager.FindByIdAsync(request.UserId);
 
-            if (user==null)
+            if (user == null)
             {
                 response.HasError = true;
                 response.Error = $"The user with this Id {request.UserId} doesn't exist";
@@ -214,7 +222,7 @@ namespace DominicanBanking.Infrastructure.Identity.Services
             await _userManager.UpdateAsync(user);
 
             return response;
-        
+
         }
 
     }
