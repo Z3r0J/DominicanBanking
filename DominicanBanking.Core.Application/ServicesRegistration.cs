4016d74c59446e91ca83e410ca24b11d416e1750
Johanly Baez <20209409@itla.edu.do>
2022-07-15 20:49:00 -0400

    GenericServices

--- DominicanBanking.Core.Application/ServicesRegistration.cs
+++ DominicanBanking.Core.Application/ServicesRegistration.cs
@@ -1,13 +1,19 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using DominicanBanking.Core.Application.Interfaces.Services;
+using DominicanBanking.Core.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Reflection;
 
 namespace DominicanBanking.Core.Application
 {
     public static class ServicesRegistration
     {
-        public static void AddApplicationLayer(this IServiceCollection service) { 
-        
-        
-        } 
+        public static void AddApplicationLayer(this IServiceCollection service) 
+        {
+            service.AddAutoMapper(Assembly.GetExecutingAssembly());
+            #region Services            
+            #endregion
+
+        }
     }
 }
